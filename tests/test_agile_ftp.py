5d8f10d53fb37ff106b29ef7e4fc1d13e3227c02
Michael Budei <77558463+MikeBlackwood@users.noreply.github.com>
2022-07-30 11:59:37 -0700

    added test-rem-dir ad if-file functions

--- tests/test_agile_ftp.py
+++ tests/test_agile_ftp.py
@@ -5,40 +5,48 @@ from agile_ftp import AgileFTP
 import ftplib
 
 class AgileFTPTests(unittest.TestCase):
-    @patch('ftplib.FTP', autospec=True)
+
 
     def test_random_ftp_server(self):
         ftp = AgileFTP()
         self.assertTrue(ftp.random_ftp_server())
 
+    @patch('ftplib.FTP', autospec=True)
     def test_connect_true(self, mock_ftp_constructor):
         mock_ftp_lib = mock_ftp_constructor.return_value
         ftp = AgileFTP('localhost', mock_ftp_lib)
         self.assertTrue(ftp.connect('localhost', 'mb', 'test'), "Returned True after Connection")
         mock_ftp_lib.login.assert_called_with('mb', 'test')
 
-
+    @patch('ftplib.FTP', autospec=True)
     def test_connect_random(self, mock_ftp_constructor):
         mock_ftp_lib = mock_ftp_constructor.return_value
         ftp = AgileFTP('localhost', mock_ftp_lib)
         self.assertTrue(ftp.connect(), "Returned True connect anonemusly")
         mock_ftp_lib.login.assert_called_with('mb', 'test')
 
-
+    @patch('ftplib.FTP', autospec=True)
     def test_disconnect_true(self, mock_ftp_constructor):
         mock_ftp_lib = mock_ftp_constructor.return_value
         ftp = AgileFTP('localhost', mock_ftp_lib)
         self.assertTrue(ftp.disconnect(), "Disconnected")
         mock_ftp_lib.quit.assert_called_once()
 
+    @patch('ftplib.FTP', autospec=True)
+    def test_rem_dir(self, mock_ftp_constructor):
+        mock_ftp_lib = mock_ftp_constructor.return_value
+        ftp = AgileFTP('localhost', mock_ftp_lib)
+        returned_val = ftp.rem_dir()
+        mock_ftp_lib.pwd.assert_called()
+        self.assertTrue(returned_val, mock_ftp_lib.pwd.return_value())
 
-    def test_rem_dir(self):
-     assert False
-
-
-    def test_is_file(self):
-
-        assert False
+    @patch('ftplib.FTP', autospec=True)
+    def test_is_file(self, mock_ftp_constructor):
+        mock_ftp_lib = mock_ftp_constructor.return_value
+        ftp = AgileFTP('localhost', mock_ftp_lib)
+        return_val = ftp.is_file('hello')
+        mock_ftp_lib.nlst.assert_called()
+        self.assertFalse(return_val, "File with name is not found")
 
 
     def test_get_files_true(self):
