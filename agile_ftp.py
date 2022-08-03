e01ce81aa7c4bc4868446b8256c2a31706e79c58
archie <archithabasavaraju@gmail.com>
2022-08-02 18:36:40 -0700

    removing progress bar

--- agile_ftp.py
+++ agile_ftp.py
@@ -119,7 +119,7 @@ class AgileFTP:
     def get_files(self, f):
         if (self.is_file(f)):
             with open(f, 'wb') as fd:
-                self._ftp.retrbinary('RETR {}'.format(f), cb)
+                self._ftp.retrbinary('RETR {}'.format(f))
             res=1
         else:
             res=0
